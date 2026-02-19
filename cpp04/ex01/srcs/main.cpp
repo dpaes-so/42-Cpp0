dec7c8308b53fd59f44169e257364f6972c034ec
Avila vm <diogo.soeiroavila@gmail.com>
2026-02-19 12:44:23 +0000

    i forgor lil things

--- cpp04/ex01/srcs/main.cpp
+++ cpp04/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: finn <finn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 10:46:50 by dpaes-so          #+#    #+#             */
-/*   Updated: 2026/02/18 20:16:05 by finn             ###   ########.fr       */
+/*   Updated: 2026/02/19 12:43:13 by finn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 
 int main() 
 {
-    const std::size_t n = 4;
+    int n = 4;
     Animal* animals[n];
 
-    for (std::size_t i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
 	{
         if (i < n / 2)
             animals[i] = new Dog();
@@ -27,12 +27,10 @@ int main()
             animals[i] = new Cat();
     }
 
-    for (std::size_t i = 0; i < n; ++i)
-	{
+    for (int i = 0; i < n; ++i)
         animals[i]->makeSound();
-    }
 
-    for (std::size_t i = 0; i < n; ++i) 
+    for (int i = 0; i < n; ++i) 
 	{
         if (Dog* d = dynamic_cast<Dog*>(animals[i])) //just a ppinter
 		{
@@ -46,7 +44,7 @@ int main()
         }
     }
 
-    for (std::size_t i = 0; i < n; ++i) 
+    for (int i = 0; i < n; ++i) 
 	{
 		std::cout << "here\n";
         if (Dog* d = dynamic_cast<Dog*>(animals[i])) 
@@ -62,20 +60,16 @@ int main()
 		std::cout << "end\n";
     }
 
-    std::cout << "\n--- Deep copy test ---\n";
     Dog original;
     original.getBrain()->setIdea(0, "Original idea");
 
-	std::cout << "div\n";
     Dog copy = original;//calls copy constucers becase copy does not exist
     copy.getBrain()->setIdea(0, "Copy idea");
 
     std::cout << "Original Dog idea[0]: " << original.getBrain()->getIdea(0) << "\n";
     std::cout << "Copy Dog idea[0]: " << copy.getBrain()->getIdea(0) << "\n";
 
-    std::cout << "\n--- Delete animals ---\n";
-    for (std::size_t i = 0; i < n; ++i) 
+    for (int i = 0; i < n; ++i) 
         delete animals[i];
-
     return 0;
 }
